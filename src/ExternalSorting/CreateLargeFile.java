b1d1024ea559a45982f55edb9dbf3f69286a70df
Farkhod Abdukodirov <abdukodirovfarkhod@gmail.com>
2023-04-14 14:38:08 +0900

    Add files via upload

---
+++ src/ExternalSorting/CreateLargeFile.java
@@ -0,0 +1,19 @@
+package org.example.SortingAlgorithms.ExternalSorting;
+
+import java.io.*;
+
+public class CreateLargeFile {
+    public static void main(String[] args) throws Exception{
+        DataOutputStream output = new DataOutputStream(
+                new BufferedOutputStream(new FileOutputStream("largedata.dat")));
+        for(int i = 0; i < 800004; i++)
+            output.writeInt((int)(Math.random() * 1000000));
+        output.close();
+
+        DataInputStream input = new DataInputStream(
+                new BufferedInputStream(new FileInputStream("largedata.dat")));
+        for(int i = 0; i < 100; i++)
+            System.out.print(input.readInt() + " ");
+        input.close();
+    }
+}
